e75e3acee12dcd383bb7dd1e3d12bbcc6f7bbd4f
kus09114 <66397141+kus09114@users.noreply.github.com>
2020-07-14 20:38:33 +0900

    delegate로 toggle group 만들기

---
+++ TestDelegate/Assets/Scripts/DelegateTestUI.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelegateTestUI : MonoBehaviour
+{
+	public static string[] CITYLIST = { "서울", "전주", "부산" };
+
+	[SerializeField] TextItem[] m_TextItems = null;
+	[SerializeField] Text m_txtResult = null;
+
+	[SerializeField] Button m_btnResult = null;
+	[SerializeField] Button m_btnClear = null;
+
+	int m_nSelectIndex = 1;
+
+	void Start()
+    {
+		m_btnResult.onClick.AddListener(OnClick_Result);
+		m_btnClear.onClick.AddListener(OnClick_Clear);
+
+		Initialize();
+    }
+
+	public void Initialize()
+	{
+		for(int i = 0; i < m_TextItems.Length; i++)
+		{
+			m_TextItems[i].OnAddListner(OnCallback_TextItem);
+			m_TextItems[i].Initialize(i, CITYLIST[i]);
+		}
+	}
+	
+	public void Initialize2()
+	{
+		for(int i = 0; i < m_TextItems.Length; i++)
+		{
+			m_TextItems[i].OnAddListner((TextItem kItem, bool bSelect) =>
+			{
+				OnCallback_TextItem(kItem, bSelect);
+			});
+		}
+	}
+
+	public void OnCallback_TextItem(TextItem kItem, bool bSelect)
+	{
+		ClearAllSelectItem();
+		kItem.SetSelectedColor(bSelect);
+		m_nSelectIndex = kItem.m_Index;
+
+		PrintResult(m_nSelectIndex);
+	}
+
+	void ClearAllSelectItem()
+	{
+		for(int i=0; i<m_TextItems.Length; i++)
+		{
+			m_TextItems[i].ClearSelect();
+		}
+	}
+
+	public void PrintResult(int nSelectIdx)
+	{
+		string sName = CITYLIST[nSelectIdx];
+		m_txtResult.text = sName;
+	}
+
+	public void OnClick_Result()
+	{
+		string sName = CITYLIST[m_nSelectIndex];
+		m_txtResult.text = string.Format("선택된 도시는 {0} 입니다.", sName);
+	}
+
+	public void OnClick_Clear()
+	{
+		m_txtResult.text = "";
+		ClearAllSelectItem();
+		m_nSelectIndex = -1;
+	}
+}
---
+++ TestDelegate/Assets/Scripts/TextItem.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextItem : MonoBehaviour
+{
+	public delegate void DelegateFunc(TextItem kItem, bool bSelect);
+	public DelegateFunc OnSelectedFunc = null;
+
+	[SerializeField] Text m_txtName = null;
+ 
+	Button m_btnSelect = null;
+	Color m_OriColor = Color.white;
+
+	public int m_Index = 0;
+
+    void Start()
+    {
+		m_OriColor = GetComponent<Image>().color;
+
+		m_btnSelect = GetComponent<Button>();
+		m_btnSelect.onClick.AddListener(OnClick_Select);
+    }
+
+	public void Initialize(int idx, string sName)
+	{
+		m_Index = idx;
+		m_txtName.text = sName;
+	}
+
+	public void OnAddListner(DelegateFunc func)
+	{
+		OnSelectedFunc = new DelegateFunc(func);
+	}
+
+	public void OnClick_Select()
+	{
+		if (OnSelectedFunc != null)
+		{
+			OnSelectedFunc(this, true);
+			Debug.Log("dd");
+		}
+	}
+
+	public void ClearSelect()
+	{
+		SetSelectedColor(false);
+	}
+
+	public void SetSelectedColor(bool bSelect)
+	{
+		Image kImage = GetComponent<Image>();
+
+		if(bSelect)
+		{
+			kImage.color = Color.green;
+		}
+		else
+		{
+			kImage.color = m_OriColor;
+		}
+	}
+}
